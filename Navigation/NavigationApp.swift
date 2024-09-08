94f9403526421db01af7cc5d858936939557806c
Denis Denisov <denis.denisov8701@gmail.com>
2024-09-08 22:13:52 +0200

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Denis Denisov on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
